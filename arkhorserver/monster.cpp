770b6d3e0f5c928d380e84b8e86748e314f92c77
Philp Masser <philip.masser@gmail.com>
2017-07-10 16:33:28 +0200

    Fix flying monsters (had wrong rules)

--- arkhorserver/monster.cpp
+++ arkhorserver/monster.cpp
@@ -98,57 +98,47 @@ void Monster::move(AH::MovementDirection dir)
         break;
 
     case Flying:
-        if (curField->type() == AH::Common::FieldData::Street) {
-            // If on street, there will be no character (this is handled above), so goto sky
-            curField = gGame->board()->field(AH::Common::FieldData::Sp_Sky);
-        } else if (curField->type() == AH::Common::FieldData::Location) {
-            // If on location, move to neighbouring street if there is a character
-            // (this should be the white AND black direction). If not, goto sky
-            Q_ASSERT_X(curField->blackField()->type() == AH::Common::FieldData::Street, "Monster::move Flying", "Black Neighbour of Location should be a street");
-            curField = curField->blackField();
-            if (!curField->hasCharacters()) {
-                curField = gGame->board()->field(AH::Common::FieldData::Sp_Sky);
-            } // Else stay
-
-        } else if (curField->type() == AH::Common::FieldData::Sky) {
-            // If in sky, find a street with character
-            QList<GameField *> streets;
-            foreach (GameField *s, gGame->board()->fields(AH::Common::FieldData::Street)) {
-                if (s->hasCharacters()) {
-                    // Candidate
-                    streets << s;
-                }
-            }
-
-            // TODO: Shuffle candidates, to make it less predictable if two characters
-            // with lowest stamina are on steets
-
-
-            // Find street with weakest character (lowest stamina)
-            GameField *r = NULL;
-            int weakest = 1000;
-            foreach (GameField *s, streets) {
-                foreach (Character *c, s->characters()) {
-                    if (c->curStamina() < weakest) {
-                        r = s;
-                        weakest = c->curStamina();
-                        break;
-                    }
-                }
-            }
-
-            if (r) {
-                curField = r;
-            }
+        // Case where monster is on a field with character already handled above.
+        // Case 1: Sky. Search all streets
+        // Case 2: Not sky. Search all connected streets
+        // Find character with minimum Sneak
+        // On tie, randomize
+    {
+        QList<GameField*> checkFields;
+        if (curField->type() == AH::Common::FieldData::Sky) {
+            checkFields = gGame->board()->fields(AH::Common::FieldData::Street);
         } else {
-            Q_ASSERT_X(false, "Monster::move flying", qPrintable(QString("Monster was on unhandled field type %1").arg(m_field->type())));
+            for (auto f : curField->neighbours().toList()) {
+                if (f->type() == AH::Common::FieldData::Street) {
+                    checkFields << f;
+                }
+            }
         }
 
-        if (curField != m_field) {
-            curField->placeMonster(this);
+        // Find field with characters of min sneak
+        int minSneak = std::numeric_limits<int>::max();
+        QSet<GameField*> candidates;
+        for (auto f : checkFields) {
+            for (auto c : f->characters()) {
+                int curSneak = gGame->context().getCharacterSkill(c, AH::Skill_Sneak).finalVal();
+                if (curSneak == minSneak) {
+                    candidates.insert(f);
+                } else if (curSneak < minSneak) {
+                    candidates.clear();
+                    minSneak = curSneak;
+                    candidates.insert(f);
+                }
+            }
         }
+
+        if (candidates.count() > 0) {
+            int i = RandomSource::instance().nextUint(0, candidates.count()-1);
+            (*(candidates.begin()+i))->placeMonster(this);
+        } else if (curField->type() != AH::Common::FieldData::Sky) {
+            gGame->board()->field(AH::Common::FieldData::Sp_Sky)->placeMonster(this);
+        }
+    }
         break;
-
     case Special:
         Q_ASSERT_X(false, "Monster::move", "Cannot handle special movement here");
     }
